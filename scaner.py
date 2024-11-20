859b2304952e022c760b2dbc0cd97bf747ad3fba
Zombi.filin <zombi.filin.256@gmail.com>
2024-11-20 16:58:06 +0300

    2024.11.20

--- scaner.py
+++ scaner.py
@@ -4,6 +4,11 @@ import urllib.request
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64)'}
 
+id_list = []
+title_list = []
+url_list = []
+price_list = []
+
 def get_html(url):
     '''
     Возвращает HTML код страницы по url
@@ -19,6 +24,22 @@ def processed_page(num):
     regex = r'>(\s)+'
     page_html = re.sub(regex, '>', page_html, 0, re.MULTILINE)
     page_list = page_html.split('><')
+    
+    product_regex = r'href=\"https://3game\.info/product/(\d+)-(.+)\"\sonclick(?:.+)>(.+)</'
+    price_regex = r'itemprop=\"price\" >([0-9]+)<'
+
+    for page_line in page_list:
+        if page_line == '/table':
+            break
+        product_find = re.findall(product_regex,page_line)
+        if len(product_find) == 1:
+            id_list.append(product_find[0][0])
+            url_list.append(f'https://3game.info/product/{product_find[0][0]}-{product_find[0][1]}')
+            title_list.append(product_find[0][2])
+        price_find = re.findall(price_regex,page_line)
+        if len(price_find)==1:
+            price_list.append(price_find[0])
+        
     pass
 
 processed_page(1)
