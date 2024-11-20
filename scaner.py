c0763ebc177de8c98e7b02b58cb2e23985a3be6d
zombi-filin <zombi.filin.256@gmail.com>
2024-11-20 23:37:38 +0300

    2024.11.20#2

--- scaner.py
+++ scaner.py
@@ -18,7 +18,8 @@ def get_html(url):
     return response.read().decode('utf-8')
 
 def processed_page(num):
-    ''' .обработка страницы под номером `num`'''
+    ''' Обработка страницы под номером `num`'''
+    print(f'Page #{num}')
     page_html = get_html(f'https://3game.info/gaming/?page={num}&alias=xbox-key')
     page_html = page_html.replace('\n','').replace('\t','')
     regex = r'>(\s)+'
@@ -26,10 +27,10 @@ def processed_page(num):
     page_list = page_html.split('><')
     
     product_regex = r'href=\"https://3game\.info/product/(\d+)-(.+)\"\sonclick(?:.+)>(.+)</'
-    price_regex = r'itemprop=\"price\" >([0-9]+)<'
+    price_regex = r'itemprop=\"price\"(?:.+)>([0-9]+)<'
 
     for page_line in page_list:
-        if page_line == '/table':
+        if page_line == 'p class="otstup-pop"':
             break
         product_find = re.findall(product_regex,page_line)
         if len(product_find) == 1:
@@ -42,5 +43,13 @@ def processed_page(num):
         
     pass
 
-processed_page(1)
-pass
+for page_num in range(1,200):
+    processed_page(page_num)
+
+
+print(f'{len(id_list)} - {len(price_list)}')
+
+f = open('out.txt', 'w', encoding='utf-8')
+for i in range(len(id_list)):
+    f.write(f'{title_list[i]}\t{price_list[i]}\n')
+f.close()
